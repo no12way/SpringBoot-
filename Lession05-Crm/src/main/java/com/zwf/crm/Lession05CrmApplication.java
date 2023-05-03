10fd99168e98737fc2984745fc5fa894a8d183df
zwf <2297736830@qq.com>
2023-05-03 17:25:43 +0800

    增加多模块

---
+++ Lession05-Crm/src/main/java/com/zwf/crm/Lession05CrmApplication.java
@@ -0,0 +1,14 @@
+package com.zwf.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//根包下面的启动类
+@SpringBootApplication
+public class Lession05CrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lession05CrmApplication.class, args);
+    }
+
+}
