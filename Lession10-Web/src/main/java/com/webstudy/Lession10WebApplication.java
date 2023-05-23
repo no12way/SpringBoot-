e933ea68e02f5b2d7b83aee8598e4f643d187c74
zwf <2297736830@qq.com>
2023-05-23 22:39:52 +0800

    测试web路径请求,风格,返回样式

---
+++ Lession10-Web/src/main/java/com/webstudy/Lession10WebApplication.java
@@ -0,0 +1,13 @@
+package com.webstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lession10WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lession10WebApplication.class, args);
+    }
+
+}
---
+++ Lession10-Web/src/test/java/com/webstudy/Lession10WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lession10WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
